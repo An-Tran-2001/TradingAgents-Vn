560b6f2f879cf17fd42f2af6cd2f21eb48f98b8a
andev <antv@tinhvan.com>
2026-06-04 22:35:49 +0700

    refactor: remove test scripts and update dependency lockfile via uv

--- test_vndirect.py
+++
@@ -1,7 +0,0 @@
-from tradingagents.dataflows.vn_vendor import get_cafef_news, get_hose_announcements, get_fiin_fundamentals
-
-print(get_cafef_news("FPT"))
-print("----------------")
-print(get_hose_announcements("FPT"))
-print("----------------")
-print(get_fiin_fundamentals("FPT"))
